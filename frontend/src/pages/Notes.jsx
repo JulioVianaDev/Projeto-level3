3e1de034b804110d38c53c3d66fd312b66fe3b95
juliovianadev <juliovianadev@gmail.com>
2023-08-17 17:35:00 -0300

    svgs

--- frontend/src/pages/Notes.jsx
+++ frontend/src/pages/Notes.jsx
@@ -1,5 +1,7 @@
 import React,{useState} from 'react'
 import "./Notes.css"
+import trash from '../svgs/trash.svg'
+import edit from '../svgs/edit.png'
 import AddNewButton from '../components/UI/AddNewButton'
 function Notes() {
   const [notes,setNotes] = useState([{title: "Muito legal",description: "eu amei one piece",date: "08/09/2023",id: 1},{title: "Muito legal",description: "eu amei one piece",date: "08/09/2023",id: 1},{title: "Muito legal",description: "eu amei one piece",date: "08/09/2023",id: 1},{title: "Muito legal",description: "eu amei one piece",date: "08/09/2023",id: 1},{title: "Muito legal",description: "eu amei one piece",date: "08/09/2023",id: 1},{title: "Muito legal",description: "eu amei one piece",date: "08/09/2023",id: 1},])
@@ -13,9 +15,9 @@ function Notes() {
                         <h1>{n.title}</h1>
                         <div className='desc'>{n.description}</div>
                       </div>
-                        <div>
-                          <div>ex</div>
-                          <div>edit</div>
+                        <div className='div-svgs'>
+                          <div><img className="svgs" src={trash}/></div>
+                          <div><img className="svgs-edit" src={edit}/></div>
                         </div>
                       </div>
         )
